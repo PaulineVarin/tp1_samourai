09c24b46b87e8f8befe9fd7bbf26d42c71f008ca
PaulineVarin <PaulineVarin@172.17.146.68>
2022-11-10 14:25:57 +0100

    Nettoyage fichier main

--- src/histoires/MonHistoire.java
+++ src/histoires/MonHistoire.java
@@ -14,12 +14,14 @@ public class MonHistoire {
 		Yakuza yakuza = new Yakuza("Yaku le noir", 42,"biere", "WarSong");
 		yakuza.direBonjour();
 		yakuza.extorquer(commercant);
-		System.out.println("argent commercant après extorquer "+commercant.getArgent());
-		System.out.println("argent yakuza apres extorquer"+yakuza.getArgent());
-		System.out.println("reputation yakuza apres extorquer"+yakuza.getReputation());
 		Ronin ronin = new Ronin("Roro", 61, "sake");
 		ronin.donner(10, commercant );
 		ronin.provoquer(yakuza);
 		ronin.direBonjour();
+		
+		//Lignes pour vérifier certains elements
+		//System.out.println("argent commercant après extorquer "+commercant.getArgent());
+		//System.out.println("argent yakuza apres extorquer"+yakuza.getArgent());
+		//System.out.println("reputation yakuza apres extorquer"+yakuza.getReputation());
 	}
 }
