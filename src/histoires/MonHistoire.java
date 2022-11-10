c769d0aa7f7b39c3526e53d5660e0b11803fe9b0
PaulineVarin <PaulineVarin@172.17.146.68>
2022-11-10 13:31:53 +0100

    Mise en place du main Humain complet

--- src/histoires/MonHistoire.java
+++ src/histoires/MonHistoire.java
@@ -6,15 +6,17 @@ import personnages.principaux.*;
 public class MonHistoire {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Humain humain = new Humain("Prof", 10, "Porto");
 		humain.direBonjour();
 		humain.boire();
 		Commercant commercant = new Commercant("Marchand", 35);
 		commercant.direBonjour();
-
-
-
+		/*Yakusa yakusa = new Yakusa("Yaku␣le␣noir", 42,"biere", "WarSong");
+		yakusa.direBonjour();
+		yakusa.extorquer(commercant);
+		Ronin ronin = new Ronin("Roro", 61, "sake");
+		ronin.donner(10, commercant );
+		ronin.provoquer(yakusa);
+		ronin.direBonjour();*/
 	}
-
 }
